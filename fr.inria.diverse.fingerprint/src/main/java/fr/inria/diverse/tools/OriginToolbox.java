609c25f6c863ab4a1130a23d3b5dd5e2a9cc30b6
Lefeuvre Romain <romain.lefeuvre@univ-rennes1.fr>
2023-02-08 17:44:43 +0100

    fix spark conf

--- fr.inria.diverse.fingerprint/src/main/java/fr/inria/diverse/tools/OriginToolbox.java
+++ fr.inria.diverse.fingerprint/src/main/java/fr/inria/diverse/tools/OriginToolbox.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
@@ -62,8 +63,10 @@ public class OriginToolbox extends SwhGraphProperties {
 	}
 
 	public void init() throws IOException {
-		spark = SparkSession.builder().appName("dataSetBuilder").config("spark.master", "local")
-				.config("spark.cores.max", Configuration.getInstance().getThreadNumber()).getOrCreate();
+		SparkConf conf = new SparkConf().setMaster("local").setAppName("dataSetBuilder")
+				.set("spark.driver.cores", "" + Configuration.getInstance().getThreadNumber())
+				.set("spark.driver.memory", "" + Runtime.getRuntime().freeMemory());
+		spark = SparkSession.builder().config(conf).getOrCreate();
 		Dataset<String> logData = spark.read().textFile("./log").cache();
 		originVisitStatus = spark.read().format("orc")
 				.load(Configuration.getInstance().getGraphPath() + "_orc_origin_visit_status/");
